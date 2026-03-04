dbc88846c1f89ed148897bb8ccc9bf24b22c4e1d
Dewmith Ranasinghe <dewmithr@zone24x7.lk>
2026-03-04 13:14:03 +0530

    This commit contains the modifications to change the DB from sqlite to Postgresql

--- device_monitor/settings.py
+++ device_monitor/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'device_monitor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "device_monitor",
+        "USER": "device_monitor_user",
+        "PASSWORD": "device_monitor_pass",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
